33eb87ca6c9582f98d8919ad8a4a4541e33ce221
Sravya Balasa <sravya.balasa@gmail.com>
2020-06-15 19:34:48 -0700

    adopt pets app to test out vue

---
+++ src/components/PetTable.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>{{ titleSpecies }} for Adoption</h1>
+        <b-table striped hover :items="pets">
+            <template v-slot:cell(name)="data">
+                <router-link :to="`/pets/${species}/${data.index}`">
+                    {{ data.value }}
+                </router-link>
+            </template>
+        </b-table>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    species: String,
+    pets: Array
+  },
+  methods: {
+    capitalizeSpecies () {
+      return this.species.charAt(0).toUpperCase() + this.species.slice(1)
+    }
+  },
+  data () {
+    return {
+      titleSpecies: this.capitalizeSpecies()
+    }
+  }
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import cats from '../data/cats'
+import dogs from '../data/dogs'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    cats,
+    dogs,
+    pets: [...cats, ...dogs]
   },
   mutations: {
+    // separate out the payload
+    appendPet: (state, { species, pet }) => {
+      state[species].push(pet)
+    }
   },
   actions: {
+    addPet: ({ commit }, payload) => {
+      commit('appendPet', payload)
+    }
   },
   modules: {
+  },
+  getters: {
+    animalsCount: (state) => {
+      return state.cats.length + state.dogs.length
+    },
+    getAllCats: (state) => {
+      return state.pets.filter((pet) => {
+        return pet.species === 'cat'
+      })
+    }
   }
 })
